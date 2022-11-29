66b8dc6990a42beea808f8703caec5ab1a74bd79
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-11-29 09:17:14 +0530

    feat: connect to db

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,13 +1,60 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = require("body-parser");
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
 const app = (0, express_1.default)();
 app.use((0, body_parser_1.urlencoded)({
     extended: false
 }));
 app.use((0, body_parser_1.json)());
-app.listen(8080, () => console.log('server is running on port 8080'));
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    if (!process.env.MONGO_URI)
+        throw new Error('MONGO_URI is expected');
+    try {
+        yield mongoose_1.default.connect(process.env.MONGO_URI);
+        console.info('Connected to DB sucessfully');
+    }
+    catch (err) {
+        throw new Error(`DB Error!`);
+    }
+    app.listen(8080, () => console.info('Server is running on port 8080 '));
+});
+start();
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import express from 'express'
 import {json, urlencoded} from 'body-parser';
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 
@@ -9,5 +13,20 @@ app.use(urlencoded({
 
 app.use(json())
 
-app.listen(8080, () => console.log('server is running on port 8080'))
+const start = async () => {
+    if(!process.env.MONGO_URI) throw new Error('MONGO_URI is expected')
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.info('Connected to DB sucessfully')
+    }
+    catch(err){
+        throw new Error(`DB Error!`)
+    }
+
+    app.listen(8080, () => console.info('Server is running on port 8080 '))
+}
+
+start()
+
 
